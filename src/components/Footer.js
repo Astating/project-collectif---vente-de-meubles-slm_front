29a6562158c7d076ab59b8287ee42dfdce357268
BaptisteGaubert <baptistegaubert18@gmail.com>
2022-03-08 16:52:30 +0100

    footer v1

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => (
+  <>
+    <div className="propos">
+      <h2>À propos</h2>
+      <p>Qui sommes nous ?</p>
+      <p>SLM company</p>
+    </div>
+    <div className="contact">
+      <h2>Contacts</h2>
+      <p>SLM@gmail.com</p>
+      <p>ECG & CGV</p>
+    </div>
+  </>
+  );
+  
+  export default Footer;
