a791b8ca72d0ac2d1abb6bbf416f99305f634ce5
BaptisteGaubert <baptistegaubert18@gmail.com>
2022-03-08 10:44:28 +0100

    new component Button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import Header from "./Header";
+import { useState } from "react";
+import React from "react";
+import Button from "./components/Button";
 
-function App() {
+const Display = ({counter}) => <div>{counter}</div>
+
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+  
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <Header name="sarah"/>
-      </header>
-    </div>
-  );
+    <>
+      <Display counter={counter}/>
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />           
+    </>
+  )
 }
 
 export default App;
