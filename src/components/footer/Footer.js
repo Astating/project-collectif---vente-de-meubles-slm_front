89704ec94c6e86ac1b83e858b5df7b13989413e6
BaptisteGaubert <baptistegaubert18@gmail.com>
2022-03-10 12:36:27 +0100

    footer presque finit

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,18 +5,12 @@ export default function Footer() {
     return (
         <>
         <div class="footer-content">
-            <h3>Foolish Developer</h3>
-            <p>Raj Template is a blog website where you will find great tutorials on web design and development. Here each tutorial is beautifully described step by step with the required source code.</p>
-            <ul class="socials">
-                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                <li><a href="#"><i class="fa fa-youtube"></i></a></li>
-                <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
-            </ul>
+            
+            <h3>About us</h3>
+            <p>we are a small company found in 2022</p>
         </div>
         <div class="footer-bottom">
-            <p>copyright &copy; <a href="#">Foolish Developer</a>  </p>
+            <p>copyright &copy; <a href="#">Salem</a></p>
                     <div class="footer-menu">
                       <ul class="f-menu">
                         <li><a href="">Home</a></li>
