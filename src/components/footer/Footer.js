dfeb28f889d975e34d20babbabb6d709f6b01b4b
BaptisteGaubert <baptistegaubert18@gmail.com>
2022-03-10 16:25:44 +0100

    footer done

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,24 +3,23 @@ import './footer.css'
 
 export default function Footer() {
     return (
-        <>
+        <div class="total-footer">
         <div class="footer-content">
             
             <h3>About us</h3>
-            <p>we are a small company found in 2022</p>
+            <p>we are a small company created in 2022</p>
         </div>
         <div class="footer-bottom">
             <p>copyright &copy; <a href="#">Salem</a></p>
                     <div class="footer-menu">
                       <ul class="f-menu">
-                        <li><a href="">Home</a></li>
-                        <li><a href="">About</a></li>
+                        <li><a href="http://localhost:3000">Home</a></li>
                         <li><a href="">Contact</a></li>
                         <li><a href="">Blog</a></li>
                       </ul>
                     </div>
         </div>
-        </>
+        </div>
     )
 
 }
