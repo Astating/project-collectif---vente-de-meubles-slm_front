f575ce0bafe3e83991562c1bec896728b1b147a1
LAPTOP-8LRM34JR\orolo <bt95240@gmail.com>
2022-03-15 15:50:30 +0100

    le panier avec bootstrap

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Button } from "react-bootstrap";
+import { Table, Button } from "react-bootstrap";
 
 export default function Cart() {
   // du code js ou autre condition blablabla
@@ -8,8 +8,10 @@ export default function Cart() {
 
   
 /*indication du nombre de produit + tableau qui donne la valeur du produit et la quantité et un bouton pour confirmé la commande */ 
-<div class="dropdown">
 
+   <div class="dropdown">
+
+<Table striped bordered hover size="sm"  class="table">
     <div id="cart">
         <p><span id="in-cart-items-num">0</span> Articles</p>
     </div>
@@ -18,17 +20,45 @@ export default function Cart() {
         <li id="empty-cart-msg"><a>Votre panier est vide</a></li>
         <li class="go-to-cart hidden"><a href="/panier/">Voir le panier</a></li>
     </ul>
-<table class="table">
     <thead>
         <tr><th>Article</th><th>Prix</th><th>Quantité</th></tr>
     </thead>
     <tbody id="cart-tablebody"></tbody>
-</table>
+</Table>
 
 <p>Sous total : <span class="subtotal"></span>€</p>
 
-<button id="confirm-command">Passer la commande</button>
+<Button variant="warning" id="confirm-command">Passer la commande</Button>
 
 </div>
+/*<Table striped bordered hover size="sm">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>Prix</th>
+      <th>quantité</th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td colSpan={2}>Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</Table>*/
         )
 } 
