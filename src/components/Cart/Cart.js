8c348dd2035791b9435259e24f3182a079f7fa2e
LAPTOP-8LRM34JR\orolo <bt95240@gmail.com>
2022-03-15 14:55:36 +0100

    le Cart sans css

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,15 +4,29 @@ import {Card,Button,} from "react-bootstrap";
 export default function Cart () {
 return (
 
-    <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-    les prix
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
+  
+/*indication du nombre de produit + tableau qui donne la valeur du produit et la quantité et un bouton pour confirmé la commande */ 
+<div class="dropdown">
+
+    <div id="cart">
+        <p><span id="in-cart-items-num">0</span> Articles</p>
+    </div>
+
+    <ul id="cart-dropdown" hidden>
+        <li id="empty-cart-msg"><a>Votre panier est vide</a></li>
+        <li class="go-to-cart hidden"><a href="/panier/">Voir le panier</a></li>
+    </ul>
+<table class="table">
+    <thead>
+        <tr><th>Article</th><th>Prix</th><th>Quantité</th></tr>
+    </thead>
+    <tbody id="cart-tablebody"></tbody>
+</table>
+
+<p>Sous total : <span class="subtotal"></span>€</p>
+
+<button id="confirm-command">Passer la commande</button>
+
+</div>
         )
-}
+} 
